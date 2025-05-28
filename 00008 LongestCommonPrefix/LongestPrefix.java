168be0dfd857948502005dc3e7c5e53e9cfad4d8
Barbara A. Loureiro <barbarakth1@gmail.com>
2025-05-28 18:20:15 -0300

    New commit

---
+++ 00008 LongestCommonPrefix/LongestPrefix.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package program.longestprefix;
+
+/**
+ *
+ * @author Barbara Angelin
+ */
+//1 <= strs.length <= 200
+//0 <= strs[i].length <= 200
+//strs[i] consists of only lowercase English letters if it is non-empty.
+public class LongestPrefix {
+
+    public static String longestCommonPrefix(String[] strs) {
+        for (int x = 0; x <= strs[0].length()-1; x++){ // for character
+            for (int y = 1; y <= strs.length-1; y++){ // for the second pararel
+                if (  x >= strs[y].length() || strs[0].charAt(x) != strs[y].charAt(x) ){
+                    if (x == 0){
+                        return "";
+                    } else if (x == 1){
+                        return String.valueOf(strs[0].charAt(0));
+                    }
+                    else {
+                        return strs[0].substring(0, x);
+                    }
+                }
+            }   
+        }       
+        return strs[0];  
+    }
+
+
+    public static void main(String[] args) {
+        String [] ex1 = {"flower","flow","flght"};
+        String [] ex2 = {"dog","racecar","car"};
+        String [] ex3 = {"ab", "a"};
+        System.out.println(longestCommonPrefix(ex3));
+        //System.out.println(ex3[1].subSequence(0, 0));
+        String result = "2";
+        ///esult+= ex1[0].charAt(0);
+       //System.out.println(ex3[0].length());
+    }
+}
